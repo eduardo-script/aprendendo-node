8e80aeb130348b9bd280af22cf95387b60604c53
Rhennan <ascomfrc@gmail.com>
2026-04-29 16:04:21 -0300

    exercicio-impar-par

---
+++ impar-par.ts
@@ -0,0 +1,9 @@
+	// Define o número para teste
+	const numero: number = 7;
+
+	// Verifica se o resto da divisão por 2 é zero
+	if (numero % 2 === 0) {
+    	console.log(`O número ${numero} é PAR.`);
+	} else {
+    	console.log(`O número ${numero} é ÍMPAR.`);
+}
