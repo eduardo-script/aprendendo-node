33dbaa68e30e2d1c8220c6ff85bef4b95d9f22cd
Rhennan <ascomfrc@gmail.com>
2026-04-29 16:24:48 -0300

    exercicio-compras

---
+++ compras.ts
@@ -0,0 +1,9 @@
+	// Estrutura de dados: Um array de strings
+	const compras: string[] = ["Café", "Pão", "Ovos", "Leite", "Manteiga"];
+
+	console.log("Minha Lista de Compras:");
+
+	// Iteração de fluxo: Percorrendo cada nó do array
+	for (const item of compras) {
+    	console.log(`- ${item}`);
+}	
