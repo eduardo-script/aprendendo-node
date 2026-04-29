6abc75f87c2f7f57c8ec7297e887f6e6313c1d68
eduardo-script <eduardovinicius.code@gmail.com>
2026-04-29 16:33:19 -0300

    Salvando progresso antes do pull

--- login.ts
+++ login.ts
@@ -1,14 +1,12 @@
 import * as readline from 'readline-sync';
 
-// 1. Definimos a senha correta (pré-definida)
 const SENHA_CORRETA: string = "1234";
 
 console.log("--- Sistema de Login ---");
 
-// 2. Pedimos para o usuário digitar a senha
 const senhaDigitada: string = readline.question("Digite a sua senha: ");
 
-// 3. Validação
+
 if (senhaDigitada === SENHA_CORRETA) {
     console.log("Acesso permitido! Bem-vindo ao sistema.");
 } else {
